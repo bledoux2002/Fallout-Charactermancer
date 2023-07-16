fa0e30d0e901dab800843dbb2ae553ff893d6404
Benjamin Ledoux <bledoux2002@gmail.com>
2023-07-16 11:54:39 -0400

    commenting
    commented purposes of functions

--- character_generator.py
+++ character_generator.py
@@ -1,6 +1,6 @@
 # Character Creator for the Fallout 2d20 Tabletop Roleplaying system by Modiphius Entertainment
 # Created by Benjamin Ledoux using the Core Rulebook February 2023 edition.
-# First created 5/27/2023 1848, last editted 7/2/2023 2314
+# First created 5/27/2023 1848, last editted 7/13/2023 1139
 
 import json
 import os
@@ -10,8 +10,8 @@ from time import sleep
     
 #    def __init__(self):
 
-#textSpeed = 0.0325
-textSpeed = 0    
+textSpeed = 0.0325
+#textSpeed = 0    
 def main():
     while True:
         printDelay('Welcome to the Fallout Tabletop Roleplaying Game Character Creator!', textSpeed, True)
@@ -38,6 +38,7 @@ def main():
                 printDelay('Entry not recognized', textSpeed, True)
                 main()
 
+#printDelay() adds a pause between each character being printed to simulate a Fallout-style terminal
 def printDelay(str, speed, newLine):
 #    strLength = len(string)
     for i in range(len(str)):
@@ -45,7 +46,8 @@ def printDelay(str, speed, newLine):
         sleep(speed)
     if newLine:
         print('')
-    
+
+#generateChar() takes the template.json character file and creates a copy to be editted
 def generateChar():
     printDelay('Please enter the file name to save the character to: ', textSpeed, False)
     fileName = input()
@@ -58,12 +60,14 @@ def generateChar():
         fileName += str(i)
     printDelay('Your character\'s file name is ' + fileName + '.json', textSpeed, True)
     main()
-        
+ 
+#listChar() gives a list of all character files saved/uploaded, and will have a view function to see each character's stats       
 def listChar():
     printDelay('Here are your currently saved characters: ', textSpeed, True)
     pass
     main()
-        
+
+#uploadChar() registers a character file from the user inputted name, afetr the file has been dropped into the program folder 
 def uploadChar():
     printDelay('Please type the name of the character file you wish to upload: ', textSpeed, False)
     charFile = input() + '.json'
