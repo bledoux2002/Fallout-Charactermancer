5a8071978c8629c37e1a5f2f80301fa3cc8532e8
Benjamin Ledoux <bledoux2002@gmail.com>
2023-08-08 12:19:04 -0400

    Settings implemented, text speed changeable
    implemented a settings json file and the ability to change the speed at which text prints out on the screen using the printDelay() function

--- character_generator.py
+++ character_generator.py
@@ -10,17 +10,22 @@ from time import sleep
     
 #    def __init__(self):
 
+#printDelay('PSYCH! Coming soon tho...', textSpeed, True)
 #textSpeed = 0.0325
-textSpeed = 0
+textSpeed = 0 #may not be necessary but could be useful in case setting.json doesn't work
+with open('settings.json', 'r') as settingsFile:
+    settingsData = json.load(settingsFile)
+    textSpeed = settingsData['tSpeed']
+
 def main():
-    textSpeed = 
-    textSpeed = 0
+    textSpeed
     printDelay('Welcome to the Fallout Tabletop Roleplaying Game Character Creator!', textSpeed, True)
-    printMenu()
-    menuChoice = input()
+    mainChoice = 0
     #While quit has not been selected, run appropriate code for selection
-    while menuChoice != '5':
-        match menuChoice:
+    while mainChoice != '5':
+        printMenu()
+        mainChoice = input()
+        match mainChoice:
             case '1':
                 fileName = generateChar()
                 editChar(fileName)
@@ -30,10 +35,10 @@ def main():
                 uploadChar()
             case '4':
                 settings()
+            case '5':
+                pass
             case _:
                 printDelay('Entry not recognized', textSpeed, True)
-        printMenu()
-        menuChoice = input()
     #Quit selected
     printDelay('Bombs dropping in ', textSpeed, False)
     printDelay('3....2....1....', 0.2, True)
@@ -63,24 +68,24 @@ def generateChar():
     printDelay('Generating new character...', textSpeed, True)
     
     # Increment filename if already in use
-    if os.path.exists(f"characters/{fileName}.json"):
+    if os.path.exists(f'characters/{fileName}.json'):
         printDelay('Character already exists, incrementing file name.', textSpeed, True)
         i = 1
-        while os.path.exists(f"characters/{fileName + str(i)}.json"):
+        while os.path.exists(f'characters/{fileName + str(i)}.json'):
             i += 1
         fileName += str(i)
-    fileName += ".json"
-    printDelay(f"Your character\'s file name is {fileName}", textSpeed, True)
+    fileName += '.json'
+    printDelay(f'Your character\'s file name is {fileName}', textSpeed, True)
     
     # Create copy of template.json w/ new fileName
-    with open("template.json", "r") as templateFile:
+    with open('template.json', 'r') as templateFile:
         #template = templateFile.read()
         #testChar = json.loads(template)
         testChar = json.load(templateFile)
-        with open(f"characters/{fileName}", "w") as charFile:
+        with open(f'characters/{fileName}', 'w') as charFile:
             json.dump(testChar, charFile, indent=4)
-            print(f"Successfully copied template to {fileName}")
-    return (f"{fileName}")
+            print(f'Successfully copied template to {fileName}')
+    return (f'{fileName}')
 
 #editChar() takes json filename as input, opens file as write file, presents menu listing contents of character sheet to edit
 def editChar(file):
@@ -98,22 +103,50 @@ def uploadChar():
 
 #settings() will present another menu for different elements of the program to change, such as text speed
 def settings():
-    selection = '0'
-    while selection != '2'
+    global textSpeed
+    setChoice = '0'
+    while setChoice != '2':
         printDelay('Please select one of the options from the menu below by enterring the corresponding number:', textSpeed, True)
         printDelay('1. Text speed', textSpeed, True)
         printDelay('2. Back', textSpeed, True)
-        selection = input()
-        match selection:
+        printDelay('Please enter the corresponding number: ', textSpeed, False)
+        setChoice = input()
+        match setChoice:
             case '1':
-                printDelay('PSYCH! Coming soon tho...')
-                changeTextSpeed()
+                printDelay('Speed options:', textSpeed, True)
+                printDelay('1. Slow', textSpeed, True)
+                printDelay('2. Medium', textSpeed, True)
+                printDelay('3. Fast', textSpeed, True)
+                printDelay('4. Instant', textSpeed, True)
+                printDelay('Current selection: ', textSpeed, False)
+                if textSpeed == 0.0625:
+                    printDelay('Slow', textSpeed, True)
+                elif textSpeed == 0.0375:
+                    printDelay('Medium', textSpeed, True)
+                elif textSpeed == 0.0125:
+                    printDelay('Fast', textSpeed, True)
+                elif textSpeed == 0: #could jsut be else instead of elif
+                    printDelay('Instant', textSpeed, True)
+                printDelay('Please enter the corresponding number: ', textSpeed, False)
+                spdChoice = input()
+                match spdChoice:
+                    case '1':
+                        textSpeed = 0.05
+                    case '2':
+                        textSpeed = 0.0375
+                    case '3':
+                        textSpeed = 0.025
+                    case '4':
+                        textSpeed = 0
+                    case _:
+                        printDelay('Sorry, that wasn\'t an option. Try again')
+                with open('settings.json', 'r+') as setFile:
+                    settingsData = json.load(setFile)
+                    settingsData['tSPeed'] = textSpeed
+            case '2':
+                pass
             case _:
-                printDelay('Oop, you done goofed. Try it again.')
-        selection = input() #necessary?
+                printDelay('Oop, you done goofed. Try it again.', textSpeed, True)
 
-def changeTextSpeed():
-    pass
-
-if __name__ == "__main__":
+if __name__ == '__main__':
     main()
